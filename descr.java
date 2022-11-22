b147cb869439471eca47a021a3e16f304f86ac40
HYDR00GEN <ovy.hydrogen@gmail.com>
2022-11-22 10:55:54 +0100

    sum

--- descr.java
+++ descr.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class descr {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Insert nr with 5 digits: ");
-        int nr = input.nextInt();
-        input.close();
-        descending(nr);
+        descending();
     }
 
-    public static void descending(int nr) {
+    public static void descending() {
 
         boolean flag = true;
         while (flag) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Insert nr with 5 digits: ");
+            int nr = input.nextInt();
+            input.close();
             if (9999 < nr && nr < 100000) {
                 flag = false;
                 for (int i = nr; i >= 150; i = i - 2) {
-                    System.out.print(i + " ,");
+                    System.out.print(i + " ");
                 }
             } else {
                 System.out.print("not a 5 digits number");
--- sum.java
+++ sum.java
@@ -1,3 +1,26 @@
-public class sum {
+import java.util.Scanner;
 
+public class sum {
+    public static void main(String[] args) {
+        getSum();
+    }
+
+    public static void getSum() {
+
+        System.out.println("Get sum of any numbers inserted inline.");
+        Scanner input = new Scanner(System.in);
+        int numbers = 0;
+        int sum = 0;
+
+        System.out.println("insert numbers here: ");
+        String line = input.nextLine();
+
+        input = new Scanner(line);
+        while (input.hasNextInt()) {
+            numbers = input.nextInt();
+            sum += numbers;
+        }
+        System.out.println("Your sum is: " + sum);
+
+    }
 }
